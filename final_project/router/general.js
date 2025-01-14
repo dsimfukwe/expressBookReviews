ff30e1935a8a117f2415872f2fa1ea8068cdda87
DS <dumisimfukwe@yahoo.com>
2025-01-14 20:53:43 +0000

    complted tasks 1 to 7

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,14 +32,22 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn],null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let booksBy = {};
+  Object.keys(books).forEach(key => {
+      if (books[key]["author"] === author){
+          booksBy = {...booksBy,[key]: books[key]};
+
+      };
+  });
+  
+  return res.send(JSON.stringify(booksBy,null,4));
 });
 
 // Get all books based on title
@@ -50,8 +58,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    return res.send(JSON.stringify(books[isbn]["reviews"],null,4));
 });
 
 module.exports.general = public_users;
